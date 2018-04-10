b3f78c163fe4dc94a37288c41d628819410ba6d7
mish <mich0202@gmail.com>
2018-04-10 16:52:38 -0400

    other files

--- old_files/herb_database.c
+++ old_files/herb_database.c
@@ -1,6 +1,7 @@
-#include <stdio.h>
 
-int herb_database(int type){
+
+
+int herb_database( int type_counter ){
 
   //this is effectively a conditional block to determine which bitmap will be sent
   //to the VGA display
@@ -8,35 +9,26 @@ int herb_database(int type){
   #define  rosemary   0
   #define  parsely    1
   #define  mint       2
-  #define  basil      3
-  #define  lavendar   4
-  #define  thyme      5
 
-  switch (type_counter)
-  ​{
+  //some bound conditions
+
+  if(type_counter < 3);
+    return
+  else if(type_counter >= 3 && type_counter < 5);
+  else;
+
+
+
       case rosemary:
           //load herb_images/rosemary.bmp;
           break;
 
-      case basil:
+      case parsely:
           //load herb_images/basil.bmp;
           break;
 
       case mint:
           //load herb_images/mint.bmp;
-          break;
-
-      case parsely:
-          //load herb_images/parsely.bmp;
-          break;
-
-      case lavendar:
-          //load herb_images/lavendar.bmp;
-          break;
-
-      case thyme:
-          //load herb_images/thyme.bmp;
-          break;
 
       default:
           printf("Unknown herb type.")
